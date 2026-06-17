dbeb90eb7f54ba055eafda1e0d7e81982c77083a
Loosiee <akunfreefire15417@gmail.com>
2026-06-17 19:35:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "Kamu orang favorit tercinta tersayangg akuuu 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
